17fc1f1e5119fc9089e04c8de75e058aedffe5b4
Akihito0943 <vnKumagae094385@gmail.com>
2026-06-28 07:50:02 +0900

    microCMSと連携

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,33 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Book from "./components/Book";
-
-// 疑似データ
-const books = [
-  {
-    id: 1,
-    title: "Book 1",
-    thumbnail: "/thumbnails/discord-clone-udemy.png",
-    price: 2980,
-    author: {
-      id: 1,
-      name: "Author 1",
-      description: "Author 1 description",
-      profile_icon: "https://source.unsplash.com/random/2",
-    },
-    content: "Content 1",
-    created_at: new Date().toString(),
-    updated_at: new Date().toString(),
-  },
-  {
-    id: 2,
-    title: "Book 2",
-    thumbnail: "/thumbnails/notion-udemy.png",
-    price: 1980,
-    author: {
-      id: 2,
-      name: "Author 2",
-      description: "Author 2 description",
-      profile_icon: "https://source.unsplash.com/random/3",
-    },
-    content: "Content 2",
-    created_at: new Date().toString(),
-    updated_at: new Date().toString(),
-  },
-  {
-    id: 3,
-    title: "Book 3",
-    price: 4980,
-    thumbnail: "/thumbnails/openai-chatapplication-udem.png",
-    author: {
-      id: 3,
-      name: "Author 3",
-      description: "Author 3 description",
-      profile_icon: "https://source.unsplash.com/random/4",
-    },
-    content: "Content 3",
-    created_at: new Date().toString(),
-    updated_at: new Date().toString(),
-  },
-  // 他の本のデータ...
-];
+import { getAllBooks } from "./lib/micro-cms/crient";
+import { BookType } from "./types/types";
 
 // eslint-disable-next-line @next/next/no-async-client-component
 export default function Home() {
+  // Bookを保持するステイト
+  const [books, setBooks] = useState<BookType[]>([]);
+
+  // microCMSからデータを取得する
+  async function getBooksFromMicroCMS() {
+    const { contents } = await getAllBooks();
+    console.log(contents);
+    setBooks(contents);
+  }
+
+  useEffect(() => {
+    getBooksFromMicroCMS();
+  }, []);
+
   return (
     <>
       <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
         <h2 className="text-center w-full font-bold text-3xl mb-2">
           Book Commerce
         </h2>
-        {books.map((book) => (
+        {books.map((book: any) => (
           <Book key={book.id} book={book} />
         ))}
       </main>
