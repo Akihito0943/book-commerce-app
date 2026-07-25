b1f5467dd22c3934aceb375204f729b07ab74aab
Akihito0943 <vnKumagae094385@gmail.com>
2026-07-26 07:27:15 +0900

    checkout/successのPOSTからresponseを削除

--- app/api/checkout/success/route.ts
+++ app/api/checkout/success/route.ts
@@ -6,7 +6,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 // 購入履歴の保存
-export async function POST(request: Request, response: Response) {
+export async function POST(request: Request) {
   // requestからセッション情報を取得する
   const { sessionId } = await request.json();
 
