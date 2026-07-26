8ed3b01e42ee62bc40307bb2939fb3afed411e63
Akihito0943 <vnKumagae094385@gmail.com>
2026-07-27 06:32:47 +0900

    MicroCMSからFetchする際にキャッシュを削除する設定を追加

--- app/lib/micro-cms/crient.ts
+++ app/lib/micro-cms/crient.ts
@@ -10,6 +10,9 @@ export const client = createClient({
 export const getAllBooks = async () => {
   const allBooks = await client.getList<BookType>({
     endpoint: "e-book",
+    customRequestInit: {
+      cache: "no-cache",
+    },
   });
 
   return allBooks;
@@ -20,6 +23,9 @@ export const getDetailBook = async (contentId: string) => {
   const detailBook = await client.getListDetail<BookType>({
     endpoint: "e-book",
     contentId,
+    customRequestInit: {
+      cache: "no-cache",
+    },
   });
 
   return detailBook;
