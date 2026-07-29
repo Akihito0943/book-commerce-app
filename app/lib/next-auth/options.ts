581ec4135e1037d0ae86ec5c7b29f09603ed0b75
Akihito0943 <vnKumagae094385@gmail.com>
2026-07-29 09:52:41 +0900

    Googleの承認を追加

--- app/lib/next-auth/options.ts
+++ app/lib/next-auth/options.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../prisma";
 
@@ -9,6 +10,11 @@ export const nextAuthOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+    }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   adapter: PrismaAdapter(prisma),
